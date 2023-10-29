90fdce7fed7cf75d98559df837a9885b8e6828fa
Yan Braga <112954340+YansBraga@users.noreply.github.com>
2023-10-29 20:33:27 -0300

    Add files via upload
    Classe tanque

---
+++ codigo/Tanque.java
@@ -0,0 +1,41 @@
+public class Tanque
+{
+    private static final double consumo = 8.2;
+    private double capacidademax;
+    private double capacidadeAtual;
+
+    public Tanque(double capacidademax, double capcacidadeAtual)
+    {
+        this.capacidademax = capacidademax;
+        this.capacidadeAtual = capcacidadeAtual;
+    }
+
+    public double abastecer(double litros)
+    {
+        if (litros<0) {
+            System.out.println("Quantidade de litros indisponível");
+        }
+        else if (litros+capacidadeAtual > capacidademax) 
+        {
+            System.out.println("A quantidade de litros excede a capacidade máxima do tanque\nSendo assim o tanque foi completado e foram desperdiçados um total de: "+ (litros+capacidadeAtual-capacidademax) +" litros de combustível");
+            capacidadeAtual = capacidademax;
+        }
+        else
+        {
+            capacidadeAtual+=litros;
+        }
+
+        return capacidadeAtual;
+    }
+
+    public double autonomiaMax()
+    {
+        return capacidademax * consumo;
+    }
+
+    public double autonomiaAtual()
+    {
+        return capacidadeAtual * consumo;
+    }
+
+}
